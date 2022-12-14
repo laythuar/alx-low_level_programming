bd031c941385d4a18b618722f0ff8732ca92939a
laythuar <engr.laythuar@yahoo.com>
2022-12-14 06:22:49 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - short description,single line
+ * 0c: contains value to be compared
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
